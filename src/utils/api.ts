1f4a3345aa998e4a17c89bc64383196a46351146
Michael Jumoc <michaeljumoc@gmail.com>
2023-09-09 20:29:33 +0800

    Fix vite build error

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -10,9 +10,11 @@ const baseUrl = `${protocol}//${hostname}${port && `:${port}`}`;
 
 const axios = setupCache(Axios);
 
+const api = import.meta.env.VITE_LOCAL_API ? `${baseUrl}/api` : import.meta.env.VITE_API_URL;
+
 export const useFetch = async (url: string = '', params: object = {}) => {
     try {
-        const { data } = await axios.get(`${baseUrl}/api${url}`, { params });
+        const { data } = await axios.get(`${api}${url}`, { params });
         return data;
     } catch (error) {
         console.log(error);
